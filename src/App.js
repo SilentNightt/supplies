c1a6727c7b80522ddbaf79a14cc2efc65ff37fbb
Stepanov Vadim <stepanov.v.b.00@mail.ru>
2024-05-04 15:22:06 +0300

    Main card content

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import './App.css';
 import React from 'react';
 import AppLayout from './components/laypot/AppLayout';
 
+
+
+
 function App() {
   return (
     <div className="App">
--- src/components/laypot/AppContent.js
+++ src/components/laypot/AppContent.js
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Flex, Layout } from 'antd';
+import AppInformation from '../content/AppInformation';
+
 
 const { Content } = Layout;
 
@@ -11,5 +13,17 @@ const contentStyle = {
   };
 
 export default function AppContent() {
-    return (<Content style={contentStyle}>Content</Content>)
+
+  const [position, setPosition] = useState([
+    {id: 1, name: 'R1000', discription: "Have smth"},
+    {id: 2, name: 'R1012', discription: "Have nothing"},
+    {id: 3, name: 'R10321', discription: "Have smth"},
+  ])
+
+    return (<Content style={contentStyle}>
+        {position.map(card => 
+          <AppInformation position = {card} key = {card.id}/>)
+        }
+        
+    </Content>)
 }
